8c0efd9ef8edfcda0dd27a605ea505bff61adee6
jkalasas <alasasjohnkyle@gmail.com>
2026-06-16 02:29:48 +0800

    feat: add tauri integration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,45 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   plugins: [react()],
+
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'es2021',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+
   test: {
     globals: true,
     environment: 'jsdom',
