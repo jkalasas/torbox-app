f0bbe080f729d8514d3bbc3484483353adc01f8a
jkalasas <alasasjohnkyle@gmail.com>
2026-06-17 00:43:55 +0800

    chore: add android build support

---
+++ src-tauri/gen/android/app/src/main/java/app/torbox/torbox/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.torbox.torbox
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
